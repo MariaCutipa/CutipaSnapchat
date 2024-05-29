cd6456f1ab9e5fa91b9cccdb3c689a0fcc1e764b
MariaCutipa <maria.c.br1031@gmail.com>
2024-05-29 12:11:58 -0500

    otro commit

--- CutipaSnapchat/iniciarSesionViewController.swift
+++ CutipaSnapchat/iniciarSesionViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBAction func buttonGoogle(_ sender: GIDSignInButton) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+          guard error == nil else {
+              print("Google Sign In error: \(error!)")
+              return  // Exit the scope
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+              print("Error obtaining user or idToken.")
+              return  // Exit the scope
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+
+          print("Inicio de sesion exitoso")
+        }
+    }
     @IBAction func iniciarSesionTapped(_ sender: Any) {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){(user,error) in print("Intentando Iniciar Sesion")
             if error != nil{
@@ -27,7 +55,8 @@ class iniciarSesionViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
-
